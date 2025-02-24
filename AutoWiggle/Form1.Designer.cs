751f77dad9ca958f1cc5647daeb1eae1a5b8f817
ThatLukeDev <luke@l2j.co.uk>
2025-02-24 19:20:02 +0000

    icon and gui

--- AutoWiggle/Form1.Designer.cs
+++ AutoWiggle/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoWiggle
 {
-    partial class Form1
+    partial class AutoWiggle
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoWiggle));
+            this.slider = new System.Windows.Forms.TrackBar();
+            this.amount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slider
+            // 
+            this.slider.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.slider.Location = new System.Drawing.Point(0, 405);
+            this.slider.Maximum = 6;
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(800, 45);
+            this.slider.TabIndex = 0;
+            this.slider.Value = 3;
+            this.slider.Scroll += new System.EventHandler(this.slider_Scroll);
+            // 
+            // amount
+            // 
+            this.amount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.amount.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amount.Location = new System.Drawing.Point(0, 0);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(800, 405);
+            this.amount.TabIndex = 1;
+            this.amount.Text = "Timer: 1 min";
+            this.amount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AutoWiggle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.slider);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutoWiggle";
+            this.Text = "AutoWiggle";
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar slider;
+        private System.Windows.Forms.Label amount;
     }
 }
 
--- AutoWiggle/Form1.cs
+++ AutoWiggle/Form1.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace AutoWiggle
 {
-    public partial class Form1 : Form
+    public partial class AutoWiggle : Form
     {
-        public Form1()
+        int frequency = 60;
+
+        public AutoWiggle()
         {
             InitializeComponent();
+        }
+
+        private void slider_Scroll(object sender, EventArgs e)
+        {
+            switch (slider.Value)
+            {
+                case 0:
+                    frequency = 5;
+                    break;
+                case 1:
+                    frequency = 10;
+                    break;
+                case 2:
+                    frequency = 30;
+                    break;
+                case 3:
+                    frequency = 60;
+                    break;
+                case 4:
+                    frequency = 120;
+                    break;
+                case 5:
+                    frequency = 300;
+                    break;
+                case 6:
+                    frequency = 600;
+                    break;
+            }
+
+            int mins = frequency / 60;
+
+            if (mins > 0)
+                amount.Text = $"Timer: {mins} mins";
+            else
+                amount.Text = $"Timer: {frequency} secs";
         }
     }
 }
